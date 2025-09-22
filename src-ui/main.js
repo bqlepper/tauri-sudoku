ce3e773f21ee6fa1a08569ed3e05643fed70313a
Brad Lepper <bqlepper@gmail.com>
2025-09-21 21:53:02 -0400

    Trying to figure out the Javascript

--- src-ui/main.js
+++ src-ui/main.js
@@ -1,18 +1,15 @@
 const { invoke } = window.__TAURI__.tauri;
 
-let greetInputEl;
-let greetMsgEl;
+let bqlMsgElement;
 
-async function greet() {
-  // Learn more about Tauri commands at https://v1.tauri.app/v1/guides/features/command
-  greetMsgEl.textContent = await invoke("greet", { name: greetInputEl.value });
+async function bqlTest() {
+    // Learn more about Tauri commands at https://v1.tauri.app/v1/guides/features/command
+    bqlMsgElement.textContent = 'Hellow from BQL!';
+    //bqlMsgElement.textContent = `Kee pressd: ${e.key}, Cell number: ${cellNumber}`;
+    //bqlMsgElement.textContent = await invoke("greet", { name: e.key });
 }
 
 window.addEventListener("DOMContentLoaded", () => {
-  greetInputEl = document.querySelector("#greet-input");
-  greetMsgEl = document.querySelector("#greet-msg");
-  document.querySelector("#greet-form").addEventListener("submit", (e) => {
-    e.preventDefault();
-    greet();
-  });
+    bqlMsgElement = document.querySelector("#bql-msg");
+    bqlMsgElement.textContent = "Ready to go!";
 });
